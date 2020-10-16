5c932e58147ba03324493a43fa24bf72fe4230fc
cutecat8110 <cutecat8110@gmail.com>
2020-10-16 14:57:19 +0800

    首頁rwd更新

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(".subnav").hover(function () {
-    $(this).children(".wapper").children("h5").toggleClass('hover_h5')
-    $(this).children(".wapper").children("h2").toggleClass('hover_h2')
+    $(this).children(".wapper").children("h5").toggleClass('hover')
+    $(this).children(".wapper").children("h2").toggleClass('hover')
     $(this).children("p").toggleClass('hover')
     $(this).parent().parent().next().children().toggleClass('hover')
     $(this).parent().parent().parent().next().toggleClass('hover')
